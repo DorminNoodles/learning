63f7afc70e7325840806b1b8a8622da26cf7da82
Loic CHETY <lchety@e1r12p13.42.fr>
2018-06-26 18:42:38 +0200

    burger

--- react/react_udemy/react-complete-guide/burger/src/components/Burger/Burger.js
+++ react/react_udemy/react-complete-guide/burger/src/components/Burger/Burger.js
@@ -1,22 +1,41 @@
 import React from 'react';
-
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-	let transformedIngredients = Object.keys(props.ingredients)
-		.map(igKey => {
-			return [...Array(props.ingredients[igKey])].map((_, i) => {
-				return <BurgerIngredient key={igKey + i} type={igKey} />;
-			});
-		})
-		.reduce((arr, el) => {
-			return arr.concat(el);
-		}, []);
-	if (transformedIngredients.length === 0) {
-		transformedIngredients = <p>Please start adding ingredients!</p>
-	}
 
+	console.log (props.ingredients);
+
+	// let transformedIngredients = Object.keys(props.ingredients).map( key => {
+	// 	var arr = [...Array(props.ingredients[key])];
+	// 	// console.log(arr)
+	// 	var modifiay = arr.map((elem, i) => {
+	// 		// console.log("hello", elem);
+	// 		return <BurgerIngredient key={key + i} type={key} />;
+	// 	})
+	// 	// console.log(arr)
+	// 	// console.log(modifiay)
+	// 	return modifiay
+	// }).reduce((accumlator, el) => {
+	// 	return accumlator.concat(el)
+	// })
+	// var reduced = []
+	// for (let elem of res) {
+	// 	reduced.push(...elem)
+	// }
+	let transformedIngredients = Object.keys(props.ingredients)
+	.map(igKey => {
+		return [...Array(props.ingredients[igKey])].map( (el, index) => {
+			console.log("elem =>", index, el);
+			return <BurgerIngredient key={igKey + index} type={igKey} />
+		})
+	}).reduce((accu, el) => {
+		return accu.concat(el);
+	}, []);
+
+	if (transformedIngredients.length === 0) {
+		transformedIngredients = <p>Please start adding ingredients !</p>
+	}
 	console.log(transformedIngredients);
 	return (
 		<div className={classes.Burger}>
