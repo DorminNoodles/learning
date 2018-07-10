672a071844c6bc663737622aa00ecd8cdc412615
Dormin <loic.chety@gmail.com>
2018-07-10 17:18:33 +0200

    tuto udemy

--- react/react_udemy/react-complete-guide/burger/src/containers/Checkout/ContactData/ContactData.js
+++ react/react_udemy/react-complete-guide/burger/src/containers/Checkout/ContactData/ContactData.js
@@ -4,58 +4,99 @@ import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css';
 import axios from '../../../axios-orders';
+import Input from '../../../components/UI/Input/Input';
 
 
 class ContactData extends Component {
 	state = {
-		name: '',
-		email: '',
-		address: {
-			street: '',
-			postalCode: ''
+		orderForm: {
+			name: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Your Name'
+				},
+				value: ''
+			},
+			street: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Street'
+				},
+				value: ''
+			},
+			zipCode: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'ZIP Code'
+				},
+				value: ''
+			},
+			country: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Country'
+				},
+				value: ''
+			},
+			email: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'email',
+					placeholder: 'Your Mail'
+				},
+				value: ''
+			},
+			deliveryMethod: {
+				elementType: 'select',
+				elementConfig: {
+					options: [
+						{value: 'fastest', displayValue: 'Fastest'},
+						{value: 'cheapest', displayValue: 'Cheapest'},
+					]
+				},
+				value: ''
+			},
 		},
 		loading: false
 	}
 
 	orderHandler = (event) => {
 		event.preventDefault();
-
 		this.setState({loading: true});
-		let order = {
+		const order = {
 			ingredients: this.props.ingredients,
-			price: this.props.price,
-			customer: {
-				name: 'Max Pouet',
-				address: {
-					street: 'Teststreet 1',
-					zipCode: '21320',
-					country: 'Germany'
-				},
-				email: 'test@test.com'
-			},
-			deliveryMethod: 'fastest'
+			price: this.props.price
 		}
-
- 		console.log(this.props.price);
- 		console.log(order.price);
-
 		axios.post('/orders.json', order)
 			.then(response => {
 				this.setState({ loading: false});
+				this.props.history.push('/');
 			})
 			.catch(error => {
-				console.log(order);
 				this.setState({ loading: false});
 			});
 	}
 
 	render () {
+		const formElementsArray = [];
+		for (let key in this.state.orderForm) {
+			formElementsArray.push({
+				id: key,
+				config: this.state.orderForm[key]
+			});
+		}
 		let form = (
 			<form>
-				<input className={classes.Input}type="text" name="name" placeholder="Your name" />
-				<input className={classes.Input}type="email" name="email" placeholder="Your Mail" />
-				<input className={classes.Input}type="text" name="street" placeholder="Street" />
-				<input className={classes.Input}type="text" name="postal" placeholder="Postal Code" />
+				{formElementsArray.map(formElement => (
+					<Input
+						elementType={formElement.config.elementType}
+						elementConfig={formElement.config.elementConfig}
+						value={formElement.config.elementConfig}/>
+				))}
 				<Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
 			</form>
 		);
